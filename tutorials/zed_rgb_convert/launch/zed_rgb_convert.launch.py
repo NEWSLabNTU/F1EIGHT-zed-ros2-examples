624ea4c640e5d2ae6739bc0af7f224ff7ded864d
aeon <jerry73204@gmail.com>
2024-07-17 13:20:41 +0800

    add F1EIGHT config

--- tutorials/zed_rgb_convert/launch/zed_rgb_convert.launch.py
+++ tutorials/zed_rgb_convert/launch/zed_rgb_convert.launch.py
@@ -1,17 +1,3 @@
-# Copyright 2024 Stereolabs
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from launch import LaunchDescription
@@ -25,13 +11,13 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # use:
-    #  - 'zed' for 'ZED' camera
-    #  - 'zedm' for 'ZED mini' camera
-    #  - 'zed2' for 'ZED2' camera
-    camera_model = 'zed2'
+    #  - 'zed' for "ZED" camera
+    #  - 'zedm' for "ZED mini" camera
+    #  - 'zed2' for "ZED2" camera
+    camera_model = 'zedxm'
 
     # Camera name
-    camera_name = 'zed2'
+    camera_name = 'zedxm'
 
     # URDF/xacro file to be loaded by the Robot State Publisher node
     xacro_path = os.path.join(
@@ -83,8 +69,8 @@ def generate_launch_description():
             config_camera,  # Camera related parameters
         ],
         remappings=[
-            ('zed_image_4ch', 'zed_node/rgb/image_rect_color'),
-            ('camera_info', 'zed_node/rgb/camera_info')
+            ("zed_image_4ch", "zed_node/rgb/image_rect_color"),
+            ("camera_info", "zed_node/rgb/camera_info")
         ]
     )
 
